b94dd843e263ff4c6a76fdc49e7a1ad0ea4548c3
Muhammad Waleed <77747037+notwld@users.noreply.github.com>
2022-12-22 08:43:41 +0000

    added lab#10

--- Lab#10/main.py
+++ Lab#10/main.py
@@ -20,37 +20,30 @@ empty = threading.Semaphore(5)
 full = threading.Semaphore(0)
 mutex = threading.Lock()
 
-def pretty_print(empty, full, num,buf):
+def pretty_print(empty, full, num):
     console = Console()
     table = Table(show_header=True, header_style="bold magenta")
-    table.add_column("Buffer", style="dim", width=12)
     table.add_column("Empty", style="dim", width=12)
     table.add_column("Full", style="dim", width=12)
-    table.add_column("Value", style="dim", width=12)
-    table.add_row(str(buf), str(empty._value), str(full._value), str(num))
+    table.add_column("Mutex", style="dim", width=12)
+    table.add_row(str(empty._value), str(full._value), str(num))
     console.print(table)
 
 def producer(pname):
-    nums = range(5)
-    global buf,empty,full
-    num = random.choice(nums)
     empty.acquire()
     mutex.acquire()         # added
-    buf.append(num)
     print("Producing",pname)
-    pretty_print(empty, full, num, buf)
+    pretty_print(empty, full, 1)
     mutex.release()         # added
     full.release()
 
 
 
 def consumer(cname):
-    global buf,empty,full
     full.acquire()
     mutex.acquire()         # added
-    num = buf.pop(0)
     print("Consuming",cname)
-    pretty_print(empty, full, num, buf)
+    pretty_print(empty, full, 1)
     mutex.release()         # added
     empty.release()
 
@@ -59,12 +52,10 @@ def consumer(cname):
 seq = ['C1','P1','P2','P3','C2','P4','P5','P6','P7']
 threads = []
 for i in seq:
-    if i[0] == 'C':
-        t = threading.Thread(target=consumer, args=(i,))
-        threads.append(t)
+    if 'C' in i:
+        threads.append(threading.Thread(target=consumer,args=(f'{i}',)))
     else:
-        t = threading.Thread(target=producer, args=(i,))
-        threads.append(t)
+        threads.append(threading.Thread(target=producer,args=(f"{i}",)))
 
 for t in threads:
     t.start()
